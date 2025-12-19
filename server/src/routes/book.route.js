471f855f531eb40d6f635717bad5d72a486193dc
deaSea34 <lenkasat3344@gmail.com>
2025-12-19 12:37:01 +0300

    rating and comments

--- server/src/routes/book.route.js
+++ server/src/routes/book.route.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const BookController = require('../controller/book.controller');
-const multer = require('multer');  
-const path = require('path');  
-const fs = require('fs');      
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 const verifyAccessToken = require('../middlewares/verifyAccessToken');
 const validateId = require('../middlewares/validateUser');
-const verifyRefreshToken = require('../middlewares/verifyRefreshToken')
+const verifyRefreshToken = require('../middlewares/verifyRefreshToken');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(__dirname, '../../uploads');
     console.log('📁 Multer путь:', uploadPath);
-    
+
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
-    
+
     cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
-  }
+  },
 });
 const upload = multer({ storage });
 
@@ -30,6 +30,16 @@ router.post('/', verifyAccessToken, upload.single('cover'), BookController.creat
 router.get('/genres', BookController.getGenres);
 // router.post('/', verifyRefreshToken, BookController.createBook);
 router.get('/:id', verifyAccessToken, validateId, BookController.getBookById);
+router.get(
+  '/:id/user-rating',
+  verifyAccessToken,
+  validateId,
+  BookController.getUserRating,
+);
+router.post('/:id/rating', verifyAccessToken, validateId, BookController.addRating);
+router.post('/:id/comments', verifyAccessToken, validateId, BookController.addComment);
+router.put('/comments/:commentId', verifyAccessToken, BookController.updateComment);
+router.delete('/comments/:commentId', verifyAccessToken, BookController.deleteComment);
 router.put('/:id', BookController.updateBook);
 router.delete('/:id', verifyAccessToken, validateId, BookController.deleteBook);
 
